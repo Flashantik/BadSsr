8aa95024143174321ce8ab4ff40e1bab6b2a7889
Anton <mushak_brest@rambler.ru>
2021-09-16 14:07:46 +0300

    Реализация SSR С багом (false-ssr)

--- components/comment.vue
+++ components/comment.vue
@@ -7,13 +7,27 @@
 
 
 <script>
+import gql from "graphql-tag"
 export default {
   name: 'CommentComponent',
    props:{
-    comment: {
-      type: Object,
-      default: ()=> {}
+    id: {
+      type: [String, Number],
+      required: true
     }
   },
+  apollo: {
+    comment: {
+      query: gql `query comment($id: ID!) {
+          comment(id: $id) {
+            id
+            name
+          }
+        }`,
+      variables() {
+        return {id: this.id}
+      }
+    },
+  }
 }
 </script>
--- pages/comments.vue
+++ pages/comments.vue
@@ -1,51 +1,14 @@
 <template>
   <div>
     <nuxt-link to="./">Назад</nuxt-link>
-    <comment :comment="comment_1" />
-    <comment :comment="comment_2" />
-    <comment :comment="comment_3" />
+    <comment :id=1 />
+    <comment :id=2 />
+    <comment :id=3 />
   </div>
 </template>
 
 <script>
-  import gql from "graphql-tag"
-  export default {
-    name: 'Comments',
-    apollo: {
-      comment_1: {
-        query: gql `query comment($id: ID!) {
-           comment_1: comment(id: $id) {
-             id
-             name
-           }
-         }`,
-        variables: {
-          id: 1
-        }
-      },
-      comment_2: {
-        query: gql `query comment($id: ID!) {
-           comment_2: comment(id: $id) {
-             id
-             name
-           }
-         }`,
-        variables: {
-          id: 2
-        }
-      },
-      comment_3: {
-        query: gql `query comment($id: ID!) {
-           comment_3: comment(id: $id) {
-             id
-             name
-           }
-         }`,
-        variables: {
-          id: 3
-        }
-      }
-    }
-  }
-
+export default {
+  name: 'Comments',
+}
 </script>
